8d6cc19c5ca37a88c8ec8ff9bd9d7ef807428fe6
unnokid <kimki0609@gmail.com>
2022-04-25 17:46:26 +0900

    Add schema

---
+++ src/main/resources/schema.sql
@@ -0,0 +1,10 @@
+CREATE TABLE products
+(
+    product_id BINARY(16) PRIMARY KEY,
+    product_name VARCHAR(20) NOT NULL,
+    category VARCHAR(50) NOT NULL ,
+    price bigint NOT NULL ,
+    description VARCHAR(500) DEFAULT NULL,
+    create_at datetime(6) NOT NULL ,
+    update_at datetime(6) DEFAULT NULL
+)
