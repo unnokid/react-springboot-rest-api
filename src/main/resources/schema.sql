12957cbac48667ea1c48c13c1bf623ad9276a406
unnokid <kimki0609@gmail.com>
2022-05-06 14:52:00 +0900

    Add table orders, order_items

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -7,4 +7,30 @@ CREATE TABLE products
     description VARCHAR(500) DEFAULT NULL,
     create_at datetime(6) NOT NULL ,
     update_at datetime(6) DEFAULT NULL
-)
+);
+
+CREATE TABLE orders
+(
+    order_id BINARY(16) PRIMARY KEY ,
+    email VARCHAR(50) NOT NULL ,
+    address VARCHAR(200) NOT NULL ,
+    postcode VARCHAR(200) NOT NULL ,
+    order_status VARCHAR(50) NOT NULL ,
+    create_at datetime(6) NOT NULL ,
+    update_at datetime(6) DEFAULT NULL
+);
+
+CREATE TABLE order_items
+(
+    seq bigint NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    order_id binary(16) NOT NULL ,
+    product_id binary(16) NOT NULL ,
+    category VARCHAR(50) NOT NULL ,
+    price bigint NOT NULL ,
+    quantity int NOT NULL ,
+    create_at datetime(6) NOT NULL ,
+    update_at datetime(6) DEFAULT NULL,
+    INDEX (order_id),
+    CONSTRAINT fk_order_items_to_order FOREIGN KEY (order_id) REFERENCES orders (order_id) ON DELETE  CASCADE,
+    CONSTRAINT fk_order_items_to_product FOREIGN KEY (product_id) REFERENCES products (product_id)
+);
